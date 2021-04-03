4190200f629ce1a18d9e49a448ea01d601ed039c
Igor Souza <igorsfdea@gmail.com>
2021-04-02 21:38:18 -0300

    Trata mensagem e clientes como especificado

--- servidor.py
+++ servidor.py
@@ -2,9 +2,8 @@
 import socket
 import datetime
 
-
 HOST = 'localhost'
-PORT = 50002
+PORT = 50000
 
 # usando TCP para o transporte de mensagens
 servico = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -19,36 +18,33 @@ arquivo = open(arq, "r")
 # criando um arquivo para cada cliente
 for x in arquivo:
     nome = x
-    nome = nome.split("\n")[0]
+    nome = nome[1:len(nome)-2]
     nomes.append(nome)
-    print(nome)
     cliente = open(nome+".txt", "w")
 
 # o servidor se conectou com um cliente e comecou a rodar
 conexao, ender = servico.accept()
-with conexao:
-    while True:
-        count = 0
-        resposta = ''
-        dataobj = ''
-        msg = conexao.recv(1024)
-        if not msg:
-            continue
-        msg = str(msg)
-        msgDividida = msg.split()
-        print(msg)
-        if msgDividida[1] not in nomes:
-            count = count + 1
-            conexao.send(str.encode("550 Address unknown"))
+while True:
+    count = 0
+    resposta = ''
+    dataobj = ''
+    msg = conexao.recv(1024).decode()
+    if not msg:
+        continue
+    msg = str(msg)
+    msgDividida = msg.split('\n')
+    if msgDividida[1] not in nomes:
+        count = count + 1
+        conexao.send(str.encode("550 Address unknown"))
 
-        # verificando se começa com uma data e termina com .
-        if not msg.startswith("b'data") or not msg.endswith(".'"):
-            count = count + 1
-            conexao.send(str.encode("500 Syntax error, command unrecognized"))
+    # verificando se começa com uma data e termina com .
+    if not msgDividida[0] == "data" or not msgDividida[len(msgDividida)-2] == '.':
+        count = count + 1
+        conexao.send(str.encode("500 Syntax error, command unrecognized"))
 
 
-        # mensagem ok, conexão com o cliente terminada
-        if count == 0:
-            open(msgDividida[1]+".txt", "a").write(msgDividida[2] + "\n")
-            conexao.send(str.encode("ok"))
-            conexao, ender = servico.accept()
+    # mensagem ok, conexão com o cliente terminada
+    if count == 0:
+        msg = '\n'.join(msgDividida[2:len(msgDividida)-2]) + '\n\n'
+        open(msgDividida[1]+".txt", "a").write(msg)
+        conexao.send(str.encode("ok"))
